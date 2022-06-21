84e87ac1a15d506fa74861c22be6a371967c9eb5
sharjeel@truemeridian.com <MHAgithub1!>
2022-06-21 15:22:38 +0500

    fix

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
-self.addEventListener('push', () => {
+self.addEventListener('push', (e) => {
+
+    var options = {
+        body : "This notification was Generated from push.",
+        icon : 'images/example.png',
+        vibrate : [100, 50 , 100],
+        data : { 
+            dateOfArrival : Date.now(),
+            primaryKey : '2'
+        },
+        actions : [{
+            action : 'explore' ,
+            title : 'Explor this new world!', 
+            icon : 'images/checkmark.png',
+        },
+        {
+            action : 'close' ,
+            title : 'close', 
+            icon : 'images/xmark.png',
+        }]
+    }
     self.registration.showNotification('Hello world!', options);
   });

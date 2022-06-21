6dbabbc6c1004bdc74a791970992c06a41c3e1bc
sharjeel@truemeridian.com <MHAgithub1!>
2022-06-21 15:04:07 +0500

    update index 3

--- push.js
+++ push.js
@@ -7,3 +7,7 @@ let vapiGeneratedKeys = {
 console.log(vapidkeys);
 
 push.setVapidDetails("mailto:poomzscript@gmail.com", vapiGeneratedKeys.publicKey, vapiGeneratedKeys.privateKey);
+
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/fMw1afgT-sc:APA91bGFPrdO2bc2D81meMTWUuUwljvSsD-Nuxe0GlU22lKzOWz7nxPDyFcgORcZTa8z5PgQWBT8sIXM4-y1YANzGw_8B3IZ1RugshjAeohtuoiYD2kFJ25STrJeZNkgLljOcDG7EAyf","expirationTime":null,"keys":{"p256dh":"BDwh1YxxdAgJRsC2O6xQpZUTS2XQQpA_TLjbKzV21J2WNEX9MDYAuhZo5jitTEKSUeg-NojaewBYup0t9fvWYto","auth":"nr3v--9xTuql3ELwiDOtTQ"}};
+
+push.sendNotification(sub, "Hello WebTalk");
